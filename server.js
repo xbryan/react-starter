6dbb8b89aa9dc95007ef1954e95e0f6675635f31
xbryan <xbryanj@gmail.com>
2015-10-11 14:54:10 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(app.get('port'), function() {
+  console.log('Listening at localhost:' + app.get('port'));
+});
